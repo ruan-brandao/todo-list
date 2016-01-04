0fe30ea08b788280b7121ea384caf0bd4d34f6dd
Ruan Brandão <ruan.bernardo@gmail.com>
2016-01-04 02:13:00 -0200

    Style top navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def login_options
     if user_signed_in?
-      link_to 'Sign out', destroy_user_session_path, method: :delete
+      link_to 'Sign out', destroy_user_session_path, method: :delete, class: "btn btn-default navbar-btn"
     else
-      link_to 'Sign in', new_user_session_path
+      link_to 'Sign in', new_user_session_path, class: "btn btn-default navbar-btn"
     end
   end
 end
